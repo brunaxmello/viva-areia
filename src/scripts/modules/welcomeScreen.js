4633b7ec823b5932c9617ab929be023ced53d6be
Cauany Nunes Rodrigues <cauanyrodrigues01@gmail.com>
2025-10-17 12:41:39 -0300

    feat: implementa lógica de busca e categorias na aplicação organizado no /modules

---
+++ src/scripts/modules/welcomeScreen.js
@@ -0,0 +1,19 @@
+/* === WELCOME SCREEN === */
+
+export function initWelcomeScreen() {
+  const exploreButton = document.getElementById("explore-button");
+  const welcomeScreen = document.getElementById("welcome-screen");
+  const mainView = document.getElementById("main-view");
+  const desktopBreakpoint = window.matchMedia("(min-width: 768px)");
+
+  exploreButton.addEventListener("click", () => {
+    if (desktopBreakpoint.matches) {
+      mainView.scrollIntoView({ behavior: "smooth" });
+      console.log("clicou");
+      console.log(mainView);
+    } else {
+      welcomeScreen.style.display = "none";
+      mainView.style.display = "block";
+    }
+  });
+}
