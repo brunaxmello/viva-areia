7213b95734082c44d31569987fed1c311ece3bce
Cauany Rodrigues <69735292+CauanyRodrigues01@users.noreply.github.com>
2026-03-05 18:17:24 -0300

    Update config.js

--- src/scripts/config/config.js
+++ src/scripts/config/config.js
@@ -1,3 +1,4 @@
-export const GOOGLE_MAPS_API_KEY = "AIzaSyDOqqB-Nwe00a1HNvHTs6t67IQ0_iqtd-U";
+export const GOOGLE_MAPS_API_KEY = "AIzaSyCUtzoTsyeyQTmjy8hrbXwX3TbXxbmtqnQ";
 
 export const MAP_ID = "cdc3026c511a3d2eca1eeb64";
+
