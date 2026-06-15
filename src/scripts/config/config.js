1d2708f32380957961b96d19cfa8a0d13e505fc9
Cauany Nunes Rodrigues <cauanyrodrigues01@gmail.com>
2026-06-15 18:12:26 -0300

    feat: adiciona c

--- src/scripts/config/config.js
+++ src/scripts/config/config.js
@@ -1,4 +1,4 @@
-export const GOOGLE_MAPS_API_KEY = "AIzaSyBNJYE8JM2YPvph8V_iKzkKkTq-kl_vUnU";
+export const GOOGLE_MAPS_API_KEY = "AIzaSyDKsnl1moq-7Otxq0auQLfe7WM70Nq8fkw";
 
 export const MAP_ID = "daadbb1e2b577d13e142a2e3";
 
